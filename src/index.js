d2275e0d7d94471cc18aac590890fc5104cd0705
mabel-gp <167148030+mabel-gp@users.noreply.github.com>
2024-07-19 22:04:32 -0500

    Incorporar contador de palabras

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 import analyzer from './analyzer.js';
+//Incorpora datos desde analyzer.js
 
 //TODO: escuchar eventos del DOM e invocar  los métodos del objeto `analyzer`
+
+// Métrica| conteo-de-palabras
+
+const result = document.querySelector('[data-testid="word-count"]');
+const words = document.querySelector('[name="user-input"]');
+words.addEventListener("input", mostrarResultados);
+
+function  mostrarResultados(){
+  result.innerHTML = analyzer.getWordCount('Palabras'+words.value);
+}
