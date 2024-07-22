dfa6437639ecca5e090f79f0de9f7f66583263f6
mabel-gp <167148030+mabel-gp@users.noreply.github.com>
2024-07-22 00:14:25 -0500

    Incorporar contador de caracteres sin espacios
    Anexar cambios en style.css

--- src/analyzer.js
+++ src/analyzer.js
@@ -6,23 +6,27 @@ const analyzer = {
 
   getCharacterCount: (text) => {
     return text.length;
-    //TODO: esta función debe retornar el recuento de caracteres que se encuentran en el parámetro `text` de tipo `string`.
   },
 
   getCharacterCountExcludingSpaces: (text) => {
-    //TODO: esta función debe retornar el recuento de caracteres excluyendo espacios y signos de puntuación que se encuentran en el parámetro `text` de tipo `string`.
+    const caracteresSinEsp = text.replace(/[^0-9a-zA-Z]/g,'');
+    if (caracteresSinEsp){
+      return caracteresSinEsp.length;
+    }else{
+      return 0;
+    }
   },
 
   getAverageWordLength: (text) => {    
-    //TODO: esta función debe retornar la longitud media de palabras que se encuentran en el parámetro `text` de tipo `string`.
+  
   },
 
   getNumberCount: (text) => {
-    //TODO: esta función debe retornar cúantos números se encuentran en el parámetro `text` de tipo `string`.
+
   },
 
   getNumberSum: (text) => {
-    //TODO: esta función debe retornar la suma de todos los números que se encuentran en el parámetro `text` de tipo `string`.
+    
   },
 };
 
--- src/index.js
+++ src/index.js
@@ -1,23 +1,32 @@
 import analyzer from './analyzer.js';
 
-//TODO: escuchar eventos del DOM e invocar  los métodos del objeto `analyzer`
+const textArea = document.querySelector('[name="user-input"]');
 
 // Métrica| conteo-de-palabras
 
 const contadorPalabras = document.querySelector('[data-testid="word-count"]');
-const palabras = document.querySelector('[name="user-input"]');
-palabras.addEventListener("input", resultadoContadorPalabras);
+textArea.addEventListener("input", resultadoContadorPalabras);
 
 function  resultadoContadorPalabras(){
-  contadorPalabras.innerHTML = 'Palabras: '+analyzer.getWordCount(palabras.value);
+  contadorPalabras.innerHTML = 'Palabras: '+analyzer.getWordCount(textArea.value);
 }
+
 
 // Métrica| conteo-de-caracteres
 
 const contadorCaracteres = document.querySelector('[data-testid="character-count"]');
-const caracteres = document.querySelector('[name="user-input"]');
-caracteres.addEventListener("input", mostrarContadorCaracteres);
+textArea.addEventListener("input", mostrarContadorCaracteres);
 
 function mostrarContadorCaracteres(){
-  contadorCaracteres.innerHTML = 'Caracteres: '+analyzer.getCharacterCount(caracteres.value);
+  contadorCaracteres.innerHTML = 'Caracteres: '+analyzer.getCharacterCount(textArea.value);
+}
+
+
+// Métrica| conteo-de-caracteres-sin-espacios
+
+const contadorCaracteresSin = document.querySelector('[data-testid="character-no-spaces-count"]');
+textArea.addEventListener("input",mostrarContadorCaracteresSin);
+
+function mostrarContadorCaracteresSin(){
+  contadorCaracteresSin.innerHTML = 'Caracteres sin espacios: '+analyzer.getCharacterCountExcludingSpaces(textArea.value);
 }
