4cbb8d372cb230370e4f4644ae5203b16eafce64
mabel-gp <167148030+mabel-gp@users.noreply.github.com>
2024-07-26 17:08:01 -0500

    Updates

---
+++ analyzer.js
@@ -0,0 +1,52 @@
+/* eslint-disable no-inner-declarations */
+const analyzer = {  
+
+  getWordCount: (text) => {
+    return text.trim().split(' ').length;
+  },
+
+  getCharacterCount: (text) => {
+    return text.length;
+  },
+
+  getCharacterCountExcludingSpaces: (text) => {
+    const caracteresSinEsp = text.replace(/[^0-9a-zA-Z]/g,'');
+    if (caracteresSinEsp){
+      return caracteresSinEsp.length;
+    }else{
+      return 0;
+    }
+  },
+
+  getAverageWordLength: (text) => { 
+    const totalCaracteres = text.replace(/[^a-zA-Z.,]/g,'').length;
+    const cantidadPalabras = text.trim().split(' ').length;
+    const longitudPromedio = totalCaracteres/cantidadPalabras;
+    return parseFloat(longitudPromedio.toFixed(2));
+  },
+
+  getNumberCount: (text) => {
+    const numeros = text.match(/\b[\d.]+\b/g);
+    if (numeros){
+      return numeros.length;
+    }else{
+      return 0;
+    }
+  },
+ 
+  getNumberSum: (text) => {
+    const soloNumeros = text.match(/\b[\d.]+\b/g);
+    let suma = 0;
+
+    if(soloNumeros === null){
+      return suma;
+    }
+
+    for(const i of soloNumeros){
+      suma = suma + parseFloat(i);
+    }
+    return suma;
+  },
+};
+
+export default analyzer;
---
+++ index.js
@@ -0,0 +1,75 @@
+import analyzer from './analyzer.js';
+
+
+const textArea = document.querySelector('[name="user-input"]');
+const contadorPalabras = document.querySelector('[data-testid="word-count"]');
+const contadorCaracteres = document.querySelector('[data-testid="character-count"]');
+const contadorCaracteresSin = document.querySelector('[data-testid="character-no-spaces-count"]');
+const longitudPromedio = document.querySelector('[data-testid="word-length-average"]');
+const contadorNumeros = document.querySelector('[data-testid="number-count"]');
+const sumaNumeros = document.querySelector('[data-testid="number-sum"]');
+
+
+// MÉTRICA| conteo-de-palabras
+
+textArea.addEventListener("input", resultadoContadorPalabras);
+function  resultadoContadorPalabras(){
+  contadorPalabras.innerHTML = 'Palabras: '+analyzer.getWordCount(textArea.value);
+}
+
+
+// MÉTRICA| conteo-de-caracteres
+
+textArea.addEventListener("input", mostrarContadorCaracteres);
+function mostrarContadorCaracteres(){
+  contadorCaracteres.innerHTML = 'Caracteres: '+analyzer.getCharacterCount(textArea.value);
+}
+
+
+// MÉTRICA| conteo-de-caracteres-sin-espacios
+
+textArea.addEventListener("input",mostrarContadorCaracteresSin);
+function mostrarContadorCaracteresSin(){
+  contadorCaracteresSin.innerHTML = 'Caracteres sin espacios: '+analyzer.getCharacterCountExcludingSpaces(textArea.value);
+}
+
+
+// MÉTRICA| longitud-promedio
+
+textArea.addEventListener("input", mostrarLongitudPromedio);
+function mostrarLongitudPromedio(){
+  longitudPromedio.innerHTML = 'Longitud promedio: '+analyzer.getAverageWordLength(textArea.value);
+}
+
+
+//MÉTRICA| números
+
+textArea.addEventListener("input",mostrarContadorNumeros);
+function mostrarContadorNumeros(){
+  contadorNumeros.innerHTML = 'Números: '+analyzer.getNumberCount(textArea.value);
+}
+
+
+//MÉTRICA| suma-de-números
+
+textArea.addEventListener("input",mostrarSumaNumeros);
+function mostrarSumaNumeros(){
+  sumaNumeros.innerHTML = 'Suma de Números: '+analyzer.getNumberSum(textArea.value);
+}
+
+
+//RESTAURAR MÉTRICAS
+
+const restaurar = document.getElementById("reset-button");
+restaurar.addEventListener("click",restaurarMetricas);
+
+function restaurarMetricas(){
+  contadorPalabras.innerHTML ='Palabras: '+0;
+  contadorCaracteres.innerHTML = 'Caracteres: '+0;
+  contadorCaracteresSin.innerHTML ='Caracteres sin espacios: '+0;
+  longitudPromedio.innerHTML = 'Longitud promedio: '+0;
+  contadorNumeros.innerHTML = 'Números: '+0;
+  sumaNumeros.innerHTML = 'Suma de Números: '+0;
+  textArea.value = '';
+}
+
